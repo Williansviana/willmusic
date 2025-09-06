8b705aa141c093fbe67304d8832d9b8e2bc941cd
seu-username <seu-email>
2025-09-05 21:10:14 -0300

    willMusic

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = 'willmusic-pro-v1';
+const ARQUIVOS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ARQUIVOS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
